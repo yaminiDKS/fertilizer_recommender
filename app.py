ab5af7bf2fe9ada50f4df169548e6bb69692d47e
yaminiDKS <113582126+yaminiDKS@users.noreply.github.com>
2025-01-05 22:53:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import google.generativeai as genai
 
 # Configure the API key for Google Generative AI
-os.environ["GEMINI_API_KEY"] = "AIzaSyBNb6Sf4w7YmdUvIUMvKyIpDvs7z3g8L4g"
+os.environ["GEMINI_API_KEY"] = ""
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
 # Create the model with the desired configuration
